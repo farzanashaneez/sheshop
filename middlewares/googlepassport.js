d542fc9e2024df4843081aa105d858bf0896a5fc
farzanashaneez <farzanashaneez@gmail.com>
2024-06-27 23:40:47 +0400

    space test

--- middlewares/googlepassport.js
+++ middlewares/googlepassport.js
@@ -5,7 +5,7 @@ const crypto=require("crypto")
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
 
-passport.use(new GoogleStrategy({
+passport.use(new GoogleStrategy({  
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL:process.env.CALLBACK_URL,
